8d22d350f0d9e6abf5404db4cf97e261e5ad0d5c
Daniel Kenji Matuo <danielkmatuo@gmail.com>
2025-05-04 20:41:33 -0300

    conserta o erro de formatação da url do site e conserta a textarea (impede de clicarem nela)

--- app_sala_furiosa/routing.py
+++ app_sala_furiosa/routing.py
@@ -4,5 +4,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^ws/chat/(?P<nome_sala>\w+)/$', consumers.SalaFuriosaConsumer.as_asgi()),
+    re_path(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.SalaFuriosaConsumer.as_asgi()),
 ]
