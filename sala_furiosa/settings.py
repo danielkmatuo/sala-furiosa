c00ddf1d66b4fce0094db23359a66ac520883a94
Daniel Kenji Matuo <danielkmatuo@gmail.com>
2025-05-04 19:21:08 -0300

    commit com mudanças no settings.py (mudança no ALLOWED_HOSTS)

--- sala_furiosa/settings.py
+++ sala_furiosa/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=@ebj=65aqmuzx+9g@i%vz#ug&0=yvtsap)jgg$wctn7j=)w+@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://sala-furiosa.onrender.com"]
 
 
 # Application definition
